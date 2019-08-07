bd4289bf1da2de690952f4322096aac5af1ff3fb
MCRcortex <s10398@ormiston.qld.edu.au>
2019-08-07 22:13:36 +1000

    Compile fix

--- src/main/java/net/cortex/clientAddon/cracker/SeedCracker.java
+++ src/main/java/net/cortex/clientAddon/cracker/SeedCracker.java
@@ -12,8 +12,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 
 import java.util.Random;
-
-import static net.earthcomputer.clientcommands.command.CrackPlayerRNGUsingItemsCommand.padLeftZeros;
 import static net.earthcomputer.clientcommands.features.EnchantmentCracker.MULTIPLIER;
 
 public class SeedCracker {
