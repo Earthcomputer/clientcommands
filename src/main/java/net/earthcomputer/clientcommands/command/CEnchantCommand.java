eebba4c5146b1bb32a95a19d6787e044e592ae43
Joe <burtonjae@hotmail.co.uk>
2019-08-14 21:00:06 +0100

    Force enchantingPrediction to be enabled for /cenchant

--- src/main/java/net/earthcomputer/clientcommands/command/CEnchantCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/CEnchantCommand.java
@@ -1,10 +1,12 @@
 package net.earthcomputer.clientcommands.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import net.earthcomputer.clientcommands.TempRules;
 import net.earthcomputer.clientcommands.features.EnchantmentCracker;
 import net.minecraft.command.CommandException;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.*;
+import net.minecraft.util.Formatting;
 
 import static net.earthcomputer.clientcommands.command.arguments.ItemAndEnchantmentsPredicateArgumentType.*;
 import static net.earthcomputer.clientcommands.command.ClientCommandManager.*;
@@ -21,6 +23,19 @@ public class CEnchantCommand {
     }
 
     private static int cenchant(ServerCommandSource source, ItemAndEnchantmentsPredicate itemAndEnchantmentsPredicate) throws CommandException {
+        if (!TempRules.getEnchantingPrediction()) {
+            Text text = new TranslatableText("commands.cenchant.needEnchantingPrediction")
+                    .formatted(Formatting.RED)
+                    .append(" ")
+                    .append(new TranslatableText("commands.cenchant.needEnchantingPrediction.enable")
+                            .styled(style -> {
+                                style.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText("/ctemprule set enchantingPrediction true")));
+                                style.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/ctemprule set enchantingPrediction true"));
+                                style.setUnderline(Boolean.TRUE);
+                            }));
+            sendFeedback(text);
+            return 0;
+        }
         EnchantmentCracker.EnchantManipulationStatus status =
                 EnchantmentCracker.manipulateEnchantments(itemAndEnchantmentsPredicate.item, itemAndEnchantmentsPredicate.predicate);
         if (status != EnchantmentCracker.EnchantManipulationStatus.OK) {
