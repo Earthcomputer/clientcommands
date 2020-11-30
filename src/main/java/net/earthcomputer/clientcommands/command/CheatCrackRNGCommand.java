372d6a2cd28a796e23eda32140c8c18eaafa83d2
Qther <dtaclanlegion@gmail.com>
2020-11-30 09:41:21 +0800

    Make ccheatcrackrng command return old seed if existent for testing

--- src/main/java/net/earthcomputer/clientcommands/command/CheatCrackRNGCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/CheatCrackRNGCommand.java
@@ -1,12 +1,14 @@
 package net.earthcomputer.clientcommands.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import net.earthcomputer.clientcommands.TempRules;
 import net.earthcomputer.clientcommands.features.PlayerRandCracker;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.TranslatableText;
 
-import static net.earthcomputer.clientcommands.command.ClientCommandManager.*;
-import static net.minecraft.server.command.CommandManager.*;
+import static net.earthcomputer.clientcommands.command.ClientCommandManager.addClientSideCommand;
+import static net.earthcomputer.clientcommands.command.ClientCommandManager.sendFeedback;
+import static net.minecraft.server.command.CommandManager.literal;
 
 public class CheatCrackRNGCommand {
 
@@ -18,8 +20,15 @@ public class CheatCrackRNGCommand {
     }
 
     private static int crackPlayerRNG(ServerCommandSource source) {
-        long seed = PlayerRandCracker.singlePlayerCrackRNG();
-        sendFeedback(new TranslatableText("commands.ccrackrng.success", Long.toHexString(seed)));
+        long seed;
+        if (TempRules.playerCrackState.knowsSeed()) {
+            long oldSeed = PlayerRandCracker.getSeed();
+            seed = PlayerRandCracker.singlePlayerCrackRNG();
+            sendFeedback(new TranslatableText("commands.ccheatcrackrng.success", Long.toHexString(oldSeed), Long.toHexString(seed)));
+        } else {
+            seed = PlayerRandCracker.singlePlayerCrackRNG();
+            sendFeedback(new TranslatableText("commands.ccrackrng.success", Long.toHexString(seed)));
+        }
         return (int) seed;
     }
 
