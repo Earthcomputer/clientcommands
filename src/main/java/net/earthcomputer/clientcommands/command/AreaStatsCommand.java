ae92d5848e7ad2f7d79eda4a5ad32b718f0b76b8
xpple <fred@xpple.dev>
2021-06-01 19:40:20 +0200

    Implement first few requested changes

--- src/main/java/net/earthcomputer/clientcommands/command/AreaStatsCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/AreaStatsCommand.java
@@ -1,5 +1,6 @@
 package net.earthcomputer.clientcommands.command;
 
+import com.google.common.collect.Streams;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
@@ -18,7 +19,6 @@ import net.minecraft.world.chunk.WorldChunk;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.StreamSupport;
 
 import static net.earthcomputer.clientcommands.command.ClientCommandManager.*;
 import static net.earthcomputer.clientcommands.command.arguments.ListArgumentType.*;
@@ -86,88 +86,83 @@ public class AreaStatsCommand {
         final BlockPos.Mutable mutablePos = new BlockPos.Mutable();
 
         int blocks = 0;
-        int chunks = 0;
+        int chunks;
 
         if (chunk1.getPos().equals(chunk2.getPos())) {
-            chunks++;
-            blocks = loop(minX, maxX, minZ, maxZ, minY, maxY, blocks, blockPredicate, chunk1, mutablePos);
+            chunks = 1;
+            blocks += loop(minX, maxX, minZ, maxZ, minY, maxY, blockPredicate, chunk1, mutablePos);
         } else if (chunk1.getPos().x == chunk2.getPos().x) {
-            chunks++;
+            chunks = 2;
             final WorldChunk minMinChunk = world.getChunk(minX >> 4, minZ >> 4);
-            blocks = loop(minX, maxX, minZ, (minZ >> 4) * 16 + 15, minY, maxY, blocks, blockPredicate, minMinChunk, mutablePos);
+            blocks += loop(minX, maxX, minZ, (minZ >> 4) * 16 + 15, minY, maxY, blockPredicate, minMinChunk, mutablePos);
 
-            chunks++;
             final WorldChunk minMaxChunk = world.getChunk(minX >> 4, maxZ >> 4);
-            blocks = loop(minX, maxX, (maxZ >> 4) * 16, maxZ, minY, maxY, blocks, blockPredicate, minMaxChunk, mutablePos);
+            blocks += loop(minX, maxX, (maxZ >> 4) * 16, maxZ, minY, maxY, blockPredicate, minMaxChunk, mutablePos);
 
             for (int chunkZ = minMinChunk.getPos().z + 1; chunkZ < minMaxChunk.getPos().z; chunkZ++) {
                 chunks++;
                 final WorldChunk chunk = world.getChunk(minMinChunk.getPos().x, chunkZ);
-                blocks = loop(minX, maxX, 16 * chunkZ, 16 * chunkZ + 15, minY, maxY, blocks, blockPredicate, chunk, mutablePos);
+                blocks += loop(minX, maxX, 16 * chunkZ, 16 * chunkZ + 15, minY, maxY, blockPredicate, chunk, mutablePos);
             }
         } else if (chunk1.getPos().z == chunk2.getPos().z) {
-            chunks++;
+            chunks = 2;
             final WorldChunk minMinChunk = world.getChunk(minX >> 4, minZ >> 4);
-            blocks = loop(minX, (minX >> 4) * 16 + 15, minZ, maxZ, minY, maxY, blocks, blockPredicate, minMinChunk, mutablePos);
+            blocks += loop(minX, (minX >> 4) * 16 + 15, minZ, maxZ, minY, maxY, blockPredicate, minMinChunk, mutablePos);
 
-            chunks++;
             final WorldChunk maxMinChunk = world.getChunk(maxX >> 4, minZ >> 4);
-            blocks = loop((maxX >> 4) * 16, maxX, minZ, maxZ, minY, maxY, blocks, blockPredicate, maxMinChunk, mutablePos);
+            blocks += loop((maxX >> 4) * 16, maxX, minZ, maxZ, minY, maxY, blockPredicate, maxMinChunk, mutablePos);
 
             for (int chunkX = minMinChunk.getPos().x + 1; chunkX < maxMinChunk.getPos().x; chunkX++) {
                 chunks++;
                 final WorldChunk chunk = world.getChunk(chunkX, minMinChunk.getPos().z);
-                blocks = loop(16 * chunkX, 16 * chunkX + 15, minZ, maxZ, minY, maxY, blocks, blockPredicate, chunk, mutablePos);
+                blocks += loop(16 * chunkX, 16 * chunkX + 15, minZ, maxZ, minY, maxY, blockPredicate, chunk, mutablePos);
             }
         } else {
+            chunks = 4;
             final WorldChunk minMinChunk, minMaxChunk, maxMinChunk, maxMaxChunk;
             minMinChunk = world.getChunk(minX >> 4, minZ >> 4);
             minMaxChunk = world.getChunk(minX >> 4, maxZ >> 4);
             maxMinChunk = world.getChunk(maxX >> 4, minZ >> 4);
             maxMaxChunk = world.getChunk(maxX >> 4, maxZ >> 4);
 
-            chunks++;
-            blocks = loop(minX, (minX >> 4) * 16 + 15, minZ, (minZ >> 4) * 16 + 15, minY, maxY, blocks, blockPredicate, minMinChunk, mutablePos);
+            blocks += loop(minX, (minX >> 4) * 16 + 15, minZ, (minZ >> 4) * 16 + 15, minY, maxY, blockPredicate, minMinChunk, mutablePos);
 
-            chunks++;
-            blocks = loop(minX, (minX >> 4) * 16 + 15, (maxZ >> 4) * 16, maxZ, minY, maxY, blocks, blockPredicate, minMaxChunk, mutablePos);
+            blocks += loop(minX, (minX >> 4) * 16 + 15, (maxZ >> 4) * 16, maxZ, minY, maxY, blockPredicate, minMaxChunk, mutablePos);
 
-            chunks++;
-            blocks = loop((maxX >> 4) * 16, maxX, minZ, (minZ >> 4) * 16 + 15, minY, maxY, blocks, blockPredicate, maxMinChunk, mutablePos);
+            blocks += loop((maxX >> 4) * 16, maxX, minZ, (minZ >> 4) * 16 + 15, minY, maxY, blockPredicate, maxMinChunk, mutablePos);
 
-            chunks++;
-            blocks = loop((maxX >> 4) * 16, maxX, (maxZ >> 4) * 16, maxZ, minY, maxY, blocks, blockPredicate, maxMaxChunk, mutablePos);
+            blocks += loop((maxX >> 4) * 16, maxX, (maxZ >> 4) * 16, maxZ, minY, maxY, blockPredicate, maxMaxChunk, mutablePos);
 
             for (int minMinMaxMin = minMinChunk.getPos().x + 1; minMinMaxMin < maxMinChunk.getPos().x; minMinMaxMin++) {
                 chunks++;
                 final WorldChunk chunk = world.getChunk(minMinMaxMin, minMinChunk.getPos().z);
-                blocks = loop(16 * minMinMaxMin, 16 * minMinMaxMin + 15, minZ, (minZ >> 4) * 16 + 15, minY, maxY, blocks, blockPredicate, chunk, mutablePos);
+                blocks += loop(16 * minMinMaxMin, 16 * minMinMaxMin + 15, minZ, (minZ >> 4) * 16 + 15, minY, maxY, blockPredicate, chunk, mutablePos);
             }
             for (int minMinMinMax = minMinChunk.getPos().z + 1; minMinMinMax < minMaxChunk.getPos().z; minMinMinMax++) {
                 chunks++;
                 final WorldChunk chunk = world.getChunk(minMinChunk.getPos().x, minMinMinMax);
-                blocks = loop(minX, (minX >> 4) * 16 + 15, 16 * minMinMinMax, 16 * minMinMinMax + 15, minY, maxY, blocks, blockPredicate, chunk, mutablePos);
+                blocks += loop(minX, (minX >> 4) * 16 + 15, 16 * minMinMinMax, 16 * minMinMinMax + 15, minY, maxY, blockPredicate, chunk, mutablePos);
             }
             for (int minMaxMaxMax = minMaxChunk.getPos().x + 1; minMaxMaxMax < maxMaxChunk.getPos().x; minMaxMaxMax++) {
                 chunks++;
                 final WorldChunk chunk = world.getChunk(minMaxMaxMax, minMaxChunk.getPos().z);
-                blocks = loop(16 * minMaxMaxMax, 16 * minMaxMaxMax + 15, (maxZ >> 4) * 16, maxZ, minY, maxY, blocks, blockPredicate, chunk, mutablePos);
+                blocks += loop(16 * minMaxMaxMax, 16 * minMaxMaxMax + 15, (maxZ >> 4) * 16, maxZ, minY, maxY, blockPredicate, chunk, mutablePos);
             }
             for (int maxMinMaxMax = maxMinChunk.getPos().z + 1; maxMinMaxMax < maxMaxChunk.getPos().z; maxMinMaxMax++) {
                 chunks++;
                 final WorldChunk chunk = world.getChunk(maxMinChunk.getPos().x, maxMinMaxMax);
-                blocks = loop((maxX >> 4) * 16, maxX, 16 * maxMinMaxMax, 16 * maxMinMaxMax + 15, minY, maxY, blocks, blockPredicate, chunk, mutablePos);
+                blocks += loop((maxX >> 4) * 16, maxX, 16 * maxMinMaxMax, 16 * maxMinMaxMax + 15, minY, maxY, blockPredicate, chunk, mutablePos);
             }
             for (int chunkX = minMinChunk.getPos().x + 1; chunkX < maxMinChunk.getPos().x; chunkX++) {
                 for (int chunkZ = minMinChunk.getPos().z + 1; chunkZ < minMaxChunk.getPos().z; chunkZ++) {
                     chunks++;
                     final WorldChunk chunk = world.getChunk(chunkX, chunkZ);
-                    blocks = loop(16 * chunkX, 16 * chunkX + 15, 16 * chunkZ, 16 * chunkZ + 15, minY, maxY, blocks, blockPredicate, chunk, mutablePos);
+                    blocks += loop(16 * chunkX, 16 * chunkX + 15, 16 * chunkZ, 16 * chunkZ + 15, minY, maxY, blockPredicate, chunk, mutablePos);
                 }
             }
         }
 
-        final long entities = StreamSupport.stream(world.getEntities().spliterator(), false)
+        final long entities = Streams.stream(world.getEntities())
                 .filter(entity ->
                         entity.getX() >= minX && entity.getX() <= maxX &&
                         entity.getZ() >= minZ && entity.getZ() <= maxZ &&
@@ -179,24 +174,25 @@ public class AreaStatsCommand {
 
         long endTime = System.nanoTime();
 
-        sendFeedback("commands.careastats.output.1", chunks, endTime - startTime, (endTime - startTime) / 1000000);
-        sendFeedback("commands.careastats.output.2", blocks, (maxX - minX + 1) * (maxY - minY + 1) * (maxZ - minZ + 1));
-        sendFeedback("commands.careastats.output.3", entities);
+        sendFeedback("commands.careastats.output.chunksScanned", chunks, endTime - startTime, (endTime - startTime) / 1000000);
+        sendFeedback("commands.careastats.output.blocksMatched", blocks, (maxX - minX + 1) * (maxY - minY + 1) * (maxZ - minZ + 1));
+        sendFeedback("commands.careastats.output.entitiesFound", entities);
 
         return blocks;
     }
 
-    private static int loop(int start1, int end1, int start2, int end2, int start3, int end3, int blocks, Predicate<Block> predicate, WorldChunk chunk, BlockPos.Mutable mutablePos) {
+    private static int loop(int start1, int end1, int start2, int end2, int start3, int end3, Predicate<Block> predicate, WorldChunk chunk, BlockPos.Mutable mutablePos) {
+        int counter = 0;
         for (int x = start1; x <= end1; x++) {
             for (int z = start2; z <= end2; z++) {
                 for (int y = start3; y <= end3; y++) {
                     if (predicate.test(chunk.getBlockState(mutablePos.set(x, y, z)).getBlock())) {
-                        blocks++;
+                        counter++;
                     }
                 }
             }
         }
-        return blocks;
+        return counter;
     }
 
     private static Predicate<Block> or(List<Predicate<Block>> predicates) {
