b7efc3c0ee46fed80b01decd563df50846c81502
xpple <fred@xpple.dev>
2021-06-03 17:31:48 +0200

    Implement video setting check

--- src/main/java/net/earthcomputer/clientcommands/command/CParticleCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/CParticleCommand.java
@@ -1,9 +1,12 @@
 package net.earthcomputer.clientcommands.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.math.Vec3d;
@@ -17,6 +20,8 @@ import static net.minecraft.command.argument.Vec3ArgumentType.*;
 import static net.minecraft.server.command.CommandManager.*;
 
 public class CParticleCommand {
+
+    private static final SimpleCommandExceptionType UNSUITABLE_PARTICLE_OPTION_EXCEPTION = new SimpleCommandExceptionType(new TranslatableText("commands.cparticle.unsuitableParticleOption"));
 
     private static final MinecraftClient client = MinecraftClient.getInstance();
 
@@ -41,13 +46,20 @@ public class CParticleCommand {
                 .executes(ctx -> spawnParticle(ctx.getSource(), getParticle(ctx, "name"), client.player.getPos(), Vec3d.ZERO, 1, 1, false))));
     }
 
-    private static int spawnParticle(ServerCommandSource source, ParticleEffect parameters, Vec3d pos, Vec3d delta, float speed, int count, boolean force) {
-
-        for (int i = 0; i < count; i++) {
-            client.world.addParticle(parameters, force, pos.x + delta.x, pos.y + delta.y, pos.z + delta.z, speed, speed, speed);
+    private static int spawnParticle(ServerCommandSource source, ParticleEffect parameters, Vec3d pos, Vec3d delta, float speed, int count, boolean force) throws CommandSyntaxException {
+        switch (client.options.particles) {
+            case MINIMAL:
+                throw UNSUITABLE_PARTICLE_OPTION_EXCEPTION.create();
+            case DECREASED:
+                if (parameters.getType() == ParticleTypes.RAIN || parameters.getType() == ParticleTypes.SMOKE) {
+                    throw UNSUITABLE_PARTICLE_OPTION_EXCEPTION.create();
+                }
+            default:
+                for (int i = 0; i < count; i++) {
+                    client.world.addParticle(parameters, force, pos.x + delta.x, pos.y + delta.y, pos.z + delta.z, speed, speed, speed);
+                }
+                sendFeedback(new TranslatableText("commands.cparticle.success", Registry.PARTICLE_TYPE.getId(parameters.getType()).toString()));
+                return 0;
         }
-
-        sendFeedback(new TranslatableText("commands.cparticle.success", Registry.PARTICLE_TYPE.getId(parameters.getType()).toString()));
-        return 0;
     }
 }
