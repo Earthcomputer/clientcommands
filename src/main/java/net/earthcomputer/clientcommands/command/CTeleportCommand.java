bbe9503bca1f39035bef935ba943bde9fdbd0da4
xpple <fred@xpple.dev>
2021-08-09 22:10:46 +0200

    Implement requested change

--- src/main/java/net/earthcomputer/clientcommands/command/CTeleportCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/CTeleportCommand.java
@@ -4,18 +4,14 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.Entity;
 import net.minecraft.network.packet.c2s.play.SpectatorTeleportC2SPacket;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.TranslatableText;
 
 import java.util.UUID;
-import java.util.stream.StreamSupport;
 
 import static net.earthcomputer.clientcommands.command.ClientCommandManager.*;
 import static net.earthcomputer.clientcommands.command.arguments.ClientEntityArgumentType.*;
-import static net.minecraft.command.CommandSource.*;
-import static net.minecraft.command.argument.UuidArgumentType.*;
 import static net.minecraft.server.command.CommandManager.*;
 
 public class CTeleportCommand {
@@ -28,12 +24,8 @@ public class CTeleportCommand {
         addClientSideCommand("ctp");
 
         dispatcher.register(literal("ctp")
-                .then(argument("uuid", uuid())
-                        .suggests((context, builder) -> suggestMatching(client.world.getPlayers().stream().map(Entity::getUuidAsString), builder))
-                        .suggests((context, builder) -> suggestMatching(StreamSupport.stream(client.world.getEntities().spliterator(), false).map(Entity::getUuidAsString), builder))
-                        .executes(ctx -> teleport(ctx.getSource(), getUuid(ctx, "uuid"))))
-                .then(argument("player", entity())
-                        .executes(ctx -> teleport(ctx.getSource(), getEntity(ctx, "player").getUuid()))));
+                .then(argument("entity", entity())
+                        .executes(ctx -> teleport(ctx.getSource(), getEntity(ctx, "entity").getUuid()))));
     }
 
     private static int teleport(ServerCommandSource source, UUID uuid) throws CommandSyntaxException {
