b4df9e39dbbd9e46006b25a74a20bed1bde80980
ImUrX <urielfontan2002@gmail.com>
2022-04-04 11:43:51 -0300

    adhering to codestyle

--- src/main/java/net/earthcomputer/clientcommands/command/PingCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/PingCommand.java
@@ -4,11 +4,10 @@ import com.mojang.brigadier.CommandDispatcher;
 import net.earthcomputer.clientcommands.features.FishingCracker;
 import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.text.TranslatableText;
 
-import static net.earthcomputer.clientcommands.command.ClientCommandHelper.sendFeedback;
-import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.literal;
+import static net.earthcomputer.clientcommands.command.ClientCommandHelper.*;
+import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.*;
 
 public class PingCommand {
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
@@ -20,10 +19,10 @@ public class PingCommand {
         MinecraftClient instance = MinecraftClient.getInstance();
         int ping = FishingCracker.getLocalPing();
 
-        if(ping == -1 || instance.isInSingleplayer()) {
-            sendFeedback(new TranslatableText("commands.client.ping.local"));
+        if (ping == -1 || instance.isInSingleplayer()) {
+            sendFeedback(new TranslatableText("commands.cping.local"));
         } else {
-            sendFeedback(new TranslatableText("commands.client.ping.multiplayer", ping));
+            sendFeedback(new TranslatableText("commands.cping.multiplayer", ping));
         }
 
         return 0;
