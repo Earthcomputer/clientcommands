e5e2bcf617fbe0ff6b57b9b538ca8b8e70a046ca
sourtaste000 <sourtaste000@i-sp.in>
2022-05-08 11:18:45 -0700

    reflectively register commands

--- src/main/java/net/earthcomputer/clientcommands/ClientCommands.java
+++ src/main/java/net/earthcomputer/clientcommands/ClientCommands.java
@@ -7,11 +7,13 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 import net.fabricmc.loader.api.FabricLoader;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Set;
 
 public class ClientCommands implements ClientModInitializer {
     private static final Logger LOGGER = LogUtils.getLogger();
@@ -30,46 +32,13 @@ public class ClientCommands implements ClientModInitializer {
     }
 
     public static void registerCommands(CommandDispatcher<FabricClientCommandSource> dispatcher) {
-        BookCommand.register(dispatcher);
-        LookCommand.register(dispatcher);
-        NoteCommand.register(dispatcher);
-        ShrugCommand.register(dispatcher);
-        FindCommand.register(dispatcher);
-        FindBlockCommand.register(dispatcher);
-        FindItemCommand.register(dispatcher);
-        TaskCommand.register(dispatcher);
-        CalcCommand.register(dispatcher);
-        TempRuleCommand.register(dispatcher);
-        RenderCommand.register(dispatcher);
-        UsageTreeCommand.register(dispatcher);
-        WikiCommand.register(dispatcher);
-        CEnchantCommand.register(dispatcher);
-        GlowCommand.register(dispatcher);
-        GetDataCommand.register(dispatcher);
-        CalcStackCommand.register(dispatcher);
-        GammaCommand.register(dispatcher);
-        MoteCommand.register(dispatcher);
-        ChorusCommand.register(dispatcher);
-        FishCommand.register(dispatcher);
-        SignSearchCommand.register(dispatcher);
-        GhostBlockCommand.register(dispatcher);
-        RelogCommand.register(dispatcher);
-        CGiveCommand.register(dispatcher);
-        CPlaySoundCommand.register(dispatcher);
-        CStopSoundCommand.register(dispatcher);
-        FovCommand.register(dispatcher);
-        HotbarCommand.register(dispatcher);
-        KitCommand.register(dispatcher);
-        ItemGroupCommand.register(dispatcher);
-        CParticleCommand.register(dispatcher);
-        PermissionLevelCommand.register(dispatcher);
-        CTellRawCommand.register(dispatcher);
-        CTimeCommand.register(dispatcher);
-        AliasCommand.register(dispatcher);
-        AreaStatsCommand.register(dispatcher);
-        CTeleportCommand.register(dispatcher);
-        PlayerInfoCommand.register(dispatcher);
-        PingCommand.register(dispatcher);
-        CrackRNGCommand.register(dispatcher);
+        Reflections reflections = new Reflections("net.earthcomputer.clientcommands.command");
+        reflections.getSubTypesOf(Object.class).forEach(clazz -> {
+            try {
+                clazz.getMethod("register", CommandDispatcher.class).invoke(null, dispatcher);
+            } catch (Exception e) {
+                LOGGER.error("Failed to register command", e);
+            }
+        });
     }
 }
