33c0d2ff69f1f902be4787a2547f645a54e34004
Joe <burtonjae@hotmail.co.uk>
2022-06-20 21:10:37 +0100

    Fix /ctp not teleporting to players outside of render distance

--- src/main/java/net/earthcomputer/clientcommands/command/CTeleportCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/CTeleportCommand.java
@@ -1,16 +1,19 @@
 package net.earthcomputer.clientcommands.command;
 
+import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.network.packet.c2s.play.SpectatorTeleportC2SPacket;
 import net.minecraft.text.Text;
 
+import java.util.Collection;
 import java.util.UUID;
 
-import static dev.xpple.clientarguments.arguments.CEntityArgumentType.*;
+import static dev.xpple.clientarguments.arguments.CGameProfileArgumentType.*;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
 
 public class CTeleportCommand {
@@ -19,14 +22,22 @@ public class CTeleportCommand {
 
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(literal("ctp")
-                .then(argument("entity", entity())
-                        .executes(ctx -> teleport(ctx.getSource(), getCEntity(ctx, "entity").getUuid()))));
+                .then(argument("entity", gameProfile())
+                        .executes(ctx -> teleport(ctx.getSource(), getCProfileArgument(ctx, "entity")))));
     }
 
-    private static int teleport(FabricClientCommandSource source, UUID uuid) throws CommandSyntaxException {
+    private static int teleport(FabricClientCommandSource source, Collection<GameProfile> gameProfiles) throws CommandSyntaxException {
         if (!source.getPlayer().isSpectator()) {
             throw NOT_SPECTATOR_EXCEPTION.create();
         }
+        if (gameProfiles.isEmpty()) {
+            throw EntityArgumentType.PLAYER_NOT_FOUND_EXCEPTION.create();
+        }
+        if (gameProfiles.size() != 1) {
+            throw EntityArgumentType.TOO_MANY_PLAYERS_EXCEPTION.create();
+        }
+        UUID uuid = gameProfiles.iterator().next().getId();
+
         source.getClient().getNetworkHandler().sendPacket(new SpectatorTeleportC2SPacket(uuid));
         source.sendFeedback(Text.translatable("commands.ctp.success", uuid.toString()));
         return Command.SINGLE_SUCCESS;
