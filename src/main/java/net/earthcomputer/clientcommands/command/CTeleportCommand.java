453d9856bf17e4dc46c6852c0016d04928bc24f2
Joe <burtonjae@hotmail.co.uk>
2022-06-21 01:09:23 +0100

    Fix /ctp not working for non-player entities

--- src/main/java/net/earthcomputer/clientcommands/command/CTeleportCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/CTeleportCommand.java
@@ -1,45 +1,48 @@
 package net.earthcomputer.clientcommands.command;
 
-import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.network.packet.c2s.play.SpectatorTeleportC2SPacket;
 import net.minecraft.text.Text;
 
-import java.util.Collection;
 import java.util.UUID;
 
-import static dev.xpple.clientarguments.arguments.CGameProfileArgumentType.*;
+import static net.earthcomputer.clientcommands.command.arguments.EntityUUIDArgumentType.*;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
 
 public class CTeleportCommand {
-
     private static final SimpleCommandExceptionType NOT_SPECTATOR_EXCEPTION = new SimpleCommandExceptionType(Text.translatable("commands.ctp.notSpectator"));
 
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(literal("ctp")
-                .then(argument("entity", gameProfile())
-                        .executes(ctx -> teleport(ctx.getSource(), getCProfileArgument(ctx, "entity")))));
+                .then(argument("entity", entityUuid())
+                        .executes(ctx -> teleport(ctx.getSource(), getEntityUuid(ctx, "entity")))));
     }
 
-    private static int teleport(FabricClientCommandSource source, Collection<GameProfile> gameProfiles) throws CommandSyntaxException {
+    private static int teleport(FabricClientCommandSource source, UUID uuid) throws CommandSyntaxException {
         if (!source.getPlayer().isSpectator()) {
             throw NOT_SPECTATOR_EXCEPTION.create();
         }
-        if (gameProfiles.isEmpty()) {
-            throw EntityArgumentType.PLAYER_NOT_FOUND_EXCEPTION.create();
-        }
-        if (gameProfiles.size() != 1) {
-            throw EntityArgumentType.TOO_MANY_PLAYERS_EXCEPTION.create();
-        }
-        UUID uuid = gameProfiles.iterator().next().getId();
 
-        source.getClient().getNetworkHandler().sendPacket(new SpectatorTeleportC2SPacket(uuid));
-        source.sendFeedback(Text.translatable("commands.ctp.success", uuid.toString()));
+        ClientPlayNetworkHandler networkHandler = source.getClient().getNetworkHandler();
+        assert networkHandler != null;
+
+        networkHandler.sendPacket(new SpectatorTeleportC2SPacket(uuid));
+
+        PlayerListEntry playerListEntry = networkHandler.getPlayerListEntry(uuid);
+        String name;
+        if (playerListEntry != null) {
+            name = playerListEntry.getProfile().getName();
+        } else {
+            name = uuid.toString();
+        }
+
+        source.sendFeedback(Text.translatable("commands.ctp.success", name));
         return Command.SINGLE_SUCCESS;
     }
 }
