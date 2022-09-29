29b4c4116a230f9a983dbcfef4ee6c20159824aa
Frederik van der Els <frederik.v.d.els@outlook.com>
2022-09-29 16:13:19 +0200

    Unregister the cplayerinfo command entirely

--- src/main/java/net/earthcomputer/clientcommands/command/PlayerInfoCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/PlayerInfoCommand.java
@@ -1,23 +1,15 @@
 package net.earthcomputer.clientcommands.command;
 
-import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.text.Text;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
 
+// Might be implemented later
 public class PlayerInfoCommand {
 
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         LiteralCommandNode<FabricClientCommandSource> cplayerinfo = dispatcher.register(literal("cplayerinfo"));
-        dispatcher.register(literal("cplayerinfo")
-                .executes(ctx -> warn(ctx.getSource())));
-    }
-
-    private static int warn(FabricClientCommandSource source) {
-        source.sendError(Text.literal("This command currently has no purpose."));
-        return Command.SINGLE_SUCCESS;
     }
 }
