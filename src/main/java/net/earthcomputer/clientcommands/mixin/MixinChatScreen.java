c7c7f2fb55ca52767209b6907dd87002e14ace5d
Frederik van der Els <frederik.v.d.els@outlook.com>
2022-10-27 15:47:32 +0200

    Fix mixin

--- src/main/java/net/earthcomputer/clientcommands/mixin/MixinChatScreen.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/MixinChatScreen.java
@@ -1,18 +1,17 @@
 package net.earthcomputer.clientcommands.mixin;
 
 import net.earthcomputer.clientcommands.command.VarCommand;
-import net.minecraft.client.gui.screen.Screen;
-import org.spongepowered.asm.mixin.Debug;
+import net.minecraft.client.gui.screen.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(Screen.class)
-public class MixinScreen {
+@Mixin(ChatScreen.class)
+public class MixinChatScreen {
 
     // replace the text before the Fabric Command API executes it,
     // but ensure the message is added to the history in its raw form.
-    @ModifyArg(method = "sendMessage(Ljava/lang/String;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;sendChatMessage(Ljava/lang/String;)V"))
+    @ModifyArg(method = "sendMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ChatPreviewer;tryConsumeResponse(Ljava/lang/String;)Lnet/minecraft/text/Text;"))
     private String onSendMessage(String message) {
         return VarCommand.replaceVariables(message);
     }
