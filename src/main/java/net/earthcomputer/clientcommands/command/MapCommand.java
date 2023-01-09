4675b2388f00adfd0c203b2f6d0ba43ef06573b0
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-01-08 20:51:47 -0700

    in world map exporting

--- src/main/java/net/earthcomputer/clientcommands/command/MapCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/MapCommand.java
@@ -6,18 +6,29 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.block.MapColor;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.texture.NativeImage;
 import net.minecraft.client.util.ScreenshotRecorder;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.decoration.ItemFrameEntity;
 import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.map.MapState;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.world.World;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static com.mojang.brigadier.arguments.IntegerArgumentType.*;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
@@ -37,40 +48,33 @@ public class MapCommand {
         );
     }
 
-    private static MapState getMapState(ClientPlayerEntity player) throws CommandSyntaxException {
-        ItemStack map;
-
-        // detect if map in hand
-        if (player.getMainHandStack().getItem() instanceof FilledMapItem) {
-            map = player.getMainHandStack();
-        } else if (player.getOffHandStack().getItem() instanceof FilledMapItem) {
-            map = player.getOffHandStack();
-        } else {
+    private static MapInfo[][] getMaps(ClientPlayerEntity player, MinecraftClient client) throws CommandSyntaxException {
+        MapInfo hand = fromHand(player);
+        if (hand != null) {
+            return new MapInfo[][]{{hand}};
+        }
+        MapInfo[][] mapInfo = fromWorld(player, client.targetedEntity);
+        if (mapInfo == null) {
             throw NO_HELD_MAP_EXCEPTION.create();
         }
-
-        Integer mapId = FilledMapItem.getMapId(map);
-        MapState mapState = FilledMapItem.getMapState(mapId, player.world);
-        if (mapState == null) {
-            throw NO_HELD_MAP_EXCEPTION.create();
-        }
-        return mapState;
+        return mapInfo;
     }
 
 
     private static int exportMap(FabricClientCommandSource source, int upscale) throws CommandSyntaxException {
-        MapState mapState = getMapState(source.getPlayer());
-        try (NativeImage image = new NativeImage(NativeImage.Format.RGBA, 128 * upscale, 128 * upscale, false)) {
-            for (int i = 0; i < 128 * upscale; i+= upscale) {
-                for (int j = 0; j < 128 * upscale; j+= upscale) {
-                    int x = i / upscale;
-                    int y = j / upscale;
-                    int color = MapColor.getRenderColor(mapState.colors[x + y * 128]);
-                    for (int k = 0; k < upscale; k++) {
-                        for (int l = 0; l < upscale; l++) {
-                            image.setColor(i + k, j + l, color);
-                        }
+        MapInfo[][] mapInfo = getMaps(source.getPlayer(), source.getClient());
+        // calculate width and height
+        int width = mapInfo[0].length * 128 * upscale;
+        int height = mapInfo.length * 128 * upscale;
+
+        try (NativeImage image = new NativeImage(NativeImage.Format.RGBA, width, height, true)) {
+            for (int i = 0; i < mapInfo.length; ++i) {
+                for (int j = 0; j < mapInfo[i].length; ++j) {
+                    MapInfo info = mapInfo[i][j];
+                    if (info == null) {
+                        continue;
                     }
+                    drawMapOffset(image, info, j * 128 * upscale, i * 128 * upscale, upscale);
                 }
             }
             File screenshotDir = new File(source.getClient().runDirectory, "screenshots");
@@ -86,6 +90,180 @@ public class MapCommand {
 
 
         return Command.SINGLE_SUCCESS;
+    }
+
+    private static void drawMapOffset(NativeImage image, MapInfo map, int xOff, int yOff, int upscale) {
+        for (int i = 0; i < 128; ++i) {
+            for (int j = 0; j < 128; ++j) {
+                int color = MapColor.getRenderColor(map.getColor(i, j));
+                for (int k = 0; k < upscale; k++) {
+                    for (int l = 0; l < upscale; l++) {
+                        image.setColor(i * upscale + k + xOff, j * upscale + l + yOff, color);
+                    }
+                }
+            }
+        }
+    }
+
+    private static MapInfo fromHand(ClientPlayerEntity player) throws CommandSyntaxException {
+        ItemStack map;
+
+        // detect if map in hand
+        if (player.getMainHandStack().getItem() instanceof FilledMapItem) {
+            map = player.getMainHandStack();
+        } else if (player.getOffHandStack().getItem() instanceof FilledMapItem) {
+            map = player.getOffHandStack();
+        } else {
+            return null;
+        }
+
+        return new MapInfo(fromItemStack(player.world, map), 0);
+    }
+
+    private static MapInfo[][] fromWorld(ClientPlayerEntity player, Entity target) {
+        if (target instanceof ItemFrameEntity frame) {
+            ItemStack map = frame.getHeldItemStack();
+            if (map == null || !(map.getItem() instanceof FilledMapItem)) {
+                return null;
+            }
+            Direction facing = frame.getHorizontalFacing();
+            // find in world
+            BlockPos pos = frame.getBlockPos();
+            FramePos current = new FramePos(pos, facing);
+            // get all loaded item frames
+            Map<FramePos, ItemFrameEntity> entities = StreamSupport.stream(player.clientWorld.getEntities().spliterator(), true).filter(e -> e instanceof ItemFrameEntity && ((ItemFrameEntity) e).getMapId().isPresent()).map(e -> (ItemFrameEntity) e).collect(Collectors.toMap(e -> new FramePos(e.getBlockPos(), e.getHorizontalFacing()), Function.identity()));
+            Set<ItemFrameEntity> connected = new HashSet<>();
+            getConnectedItemFrames(connected, entities, current);
+            // put them in proper traversal order
+            Direction yAxis = switch (facing) {
+                case NORTH, SOUTH, EAST, WEST -> Direction.DOWN;
+                case UP -> player.getHorizontalFacing().getOpposite();
+                case DOWN -> player.getHorizontalFacing();
+            };
+            Direction xAxis = switch (facing) {
+                case NORTH -> Direction.WEST;
+                case EAST -> Direction.NORTH;
+                case SOUTH -> Direction.EAST;
+                case WEST -> Direction.SOUTH;
+                case UP -> Direction.fromHorizontal((yAxis.getHorizontal() + 3) % 4);
+                case DOWN -> Direction.fromHorizontal((yAxis.getHorizontal() + 1) % 4);
+            };
+            Map<Vec2i, ItemFrameEntity> flattened = new HashMap<>();
+            Iterator<ItemFrameEntity> iterator = connected.iterator();
+            ItemFrameEntity origin = iterator.next();
+            flattened.put(new Vec2i(0, 0), origin);
+            BlockPos zero = origin.getBlockPos();
+            while (iterator.hasNext()) {
+                ItemFrameEntity next = iterator.next();
+                // calculate offset in 2d from 0 0
+                BlockPos nextPos = next.getBlockPos();
+                Vec3i vec = nextPos.subtract(zero);
+                int y = vec.getComponentAlongAxis(yAxis.getAxis()) * yAxis.getDirection().offset();
+                int x = vec.getComponentAlongAxis(xAxis.getAxis()) * xAxis.getDirection().offset();
+                // this should never be possible
+                if (flattened.put(new Vec2i(x, y), next) != null) throw new IllegalStateException("Duplicate item frame at " + nextPos);
+            }
+            System.out.println(flattened);
+            // find min and max
+            Iterator<Vec2i> vec2iIterator = flattened.keySet().iterator();
+            Vec2i first = vec2iIterator.next();
+            int minX = first.x();
+            int maxX = first.x();
+            int minY = first.z();
+            int maxY = first.z();
+            while (vec2iIterator.hasNext()) {
+                Vec2i next = vec2iIterator.next();
+                minX = Math.min(minX, next.x());
+                maxX = Math.max(maxX, next.x());
+                minY = Math.min(minY, next.z());
+                maxY = Math.max(maxY, next.z());
+            }
+            // create array
+            MapInfo[][] mapInfo = new MapInfo[maxY - minY + 1][maxX - minX + 1];
+            for (Map.Entry<Vec2i, ItemFrameEntity> entry : flattened.entrySet()) {
+                Vec2i pos2 = entry.getKey();
+                ItemFrameEntity entity = entry.getValue();
+                int rotationOffset = 0;
+                if (yAxis != Direction.DOWN) {
+                    rotationOffset = switch (yAxis) {
+                        case WEST -> 3;
+                        case EAST -> 1;
+                        case SOUTH -> facing == Direction.UP ? 0 : 2;
+                        default -> facing == Direction.UP ? 2 : 0;
+                    };
+                }
+                mapInfo[pos2.z() - minY][pos2.x() - minX] = new MapInfo(fromItemFrame(entry.getValue()), entity.getRotation() + rotationOffset);
+            }
+            return mapInfo;
+        }
+        return null;
+    }
+
+    private static void getConnectedItemFrames(Set<ItemFrameEntity> already, Map<FramePos, ItemFrameEntity> frames, FramePos current) {
+        if (frames.containsKey(current)) {
+            ItemFrameEntity frame = frames.get(current);
+            if (!already.contains(frame)) {
+                already.add(frame);
+                BlockPos[] adjacent = switch (current.facing().getAxis()) {
+                    case X -> new BlockPos[]{current.pos().north(), current.pos().south(), current.pos().up(), current.pos().down()};
+                    case Y -> new BlockPos[]{current.pos().north(), current.pos().south(), current.pos().east(), current.pos().west()};
+                    case Z -> new BlockPos[]{current.pos().east(), current.pos().west(), current.pos().up(), current.pos().down()};
+                };
+                for (BlockPos pos : adjacent) {
+                    getConnectedItemFrames(already, frames, new FramePos(pos, current.facing()));
+                }
+            }
+        }
+    }
+
+    private static MapState fromItemStack(World world, ItemStack map) throws CommandSyntaxException {
+        Integer mapId = FilledMapItem.getMapId(map);
+        MapState mapState = FilledMapItem.getMapState(mapId, world);
+        if (mapState == null) {
+            throw NO_HELD_MAP_EXCEPTION.create();
+        }
+
+        return mapState;
+    }
+
+    private static MapState fromItemFrame(ItemFrameEntity entity) {
+        Integer mapId = entity.getMapId().orElseThrow();
+        return FilledMapItem.getMapState(mapId, entity.world);
+    }
+
+    private record MapInfo(MapState state, int rotation) {
+
+        public int getColor(int x, int y) {
+            // rotate x/y
+            switch (rotation % 4) {
+                case 0 -> {
+                    return state.colors[x + y * 128];
+                }
+                case 1 -> {
+                    // 90 clockwise
+                    int newX = y;
+                    int newY = 127 - x;
+                    return state.colors[newX + newY * 128];
+                }
+                case 2 -> {
+                    // 180 clockwise
+                    int newX = 127 - x;
+                    int newY = 127 - y;
+                    return state.colors[newX + newY * 128];
+                }
+                case 3 -> {
+                    // 270 clockwise
+                    int newX = 127 - y;
+                    int newY = x;
+                    return state.colors[newX + newY * 128];
+                }
+                // this should never be possible
+                default -> throw new IllegalStateException("Unexpected value: " + rotation);
+            }
+        }
+    }
+
+    private record FramePos(BlockPos pos, Direction facing) {
     }
 
 }
