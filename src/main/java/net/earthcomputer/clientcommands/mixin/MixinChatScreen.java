d465e69e96dad6591644221a8cbbabc55f352c51
Katie <72476111+RealRTTV@users.noreply.github.com>
2024-04-27 08:03:51 -0400

    Fix #627 + Fix issue with pasting client command truncating at 256 chars. (#628)

--- src/main/java/net/earthcomputer/clientcommands/mixin/MixinChatScreen.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/MixinChatScreen.java
@@ -2,24 +2,26 @@ package net.earthcomputer.clientcommands.mixin;
 
 import net.earthcomputer.clientcommands.ClientCommands;
 import net.earthcomputer.clientcommands.command.VarCommand;
+import net.earthcomputer.clientcommands.interfaces.IEditBox;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.ChatScreen;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.util.StringUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatScreen.class)
 public class MixinChatScreen {
     @Shadow protected EditBox input;
 
-    @Unique
-    @Nullable
-    private Integer oldMaxLength = null;
+    @Inject(method = "init", at = @At("TAIL"))
+    private void init(CallbackInfo ci) {
+        ((IEditBox) input).clientcommands_setClientCommandLengthExtension();
+    }
 
     // replace the text before the Fabric Command API executes it,
     // but ensure the message is added to the history in its raw form.
@@ -32,24 +34,8 @@ public class MixinChatScreen {
         return command;
     }
 
-    @Inject(method = "onEdited", at = @At("HEAD"))
-    private void onEdited(String value, CallbackInfo ci) {
-        boolean isClientcommandsCommand = false;
-        if (value.startsWith("/")) {
-            String[] commandArgs = value.substring(1).split(" ");
-            isClientcommandsCommand = commandArgs.length > 0 && ClientCommands.isClientcommandsCommand(commandArgs[0]);
-        }
-        if (isClientcommandsCommand) {
-            if (oldMaxLength == null) {
-                oldMaxLength = input.maxLength;
-            }
-            input.setMaxLength(32767);
-        } else {
-            // TODO: what if other mods try to do the same thing?
-            if (oldMaxLength != null) {
-                input.setMaxLength(oldMaxLength);
-                oldMaxLength = null;
-            }
-        }
+    @Redirect(method = "normalizeChatMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/StringUtil;trimChatMessage(Ljava/lang/String;)Ljava/lang/String;"))
+    private String normalizeChatMessage(String string) {
+        return StringUtil.truncateStringIfNecessary(string, input.maxLength, false);
     }
 }
--- src/main/java/net/earthcomputer/clientcommands/mixin/MixinEditBox.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/MixinEditBox.java
@@ -1,14 +1,88 @@
 package net.earthcomputer.clientcommands.mixin;
 
+import net.earthcomputer.clientcommands.ClientCommands;
 import net.earthcomputer.clientcommands.interfaces.IEditBox;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.util.StringUtil;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Predicate;
 
 @Mixin(EditBox.class)
 public abstract class MixinEditBox implements IEditBox {
+    @Shadow
+    private int cursorPos;
+    @Shadow
+    private int highlightPos;
 
-    @Accessor("maxLength")
+    @Shadow public abstract void setMaxLength(int length);
+
+    @Shadow public int maxLength;
+
+    @Shadow public abstract String getValue();
+
+    @Shadow private Predicate<String> filter;
+    @Unique
+    @Nullable
+    private Integer oldMaxLength = null;
+    @Unique
+    private boolean clientCommandLengthExtension;
+
     @Override
-    public abstract int clientcommands_getMaxLength();
+    public void clientcommands_setClientCommandLengthExtension() {
+        clientCommandLengthExtension = true;
+    }
+
+    @Inject(method = "insertText", at = @At("HEAD"))
+    private void onInsertText(String textToWrite, CallbackInfo ci) {
+        int startSelection = Math.min(cursorPos, highlightPos);
+        int endSelection = Math.max(cursorPos, highlightPos);
+        String newText = new StringBuilder(getValue()).replace(startSelection, endSelection, StringUtil.filterText(textToWrite)).toString();
+        if (!this.filter.test(newText)) {
+            return;
+        }
+
+        updateTextMaxLength(newText);
+    }
+
+    @Inject(method = "setValue", at = @At("HEAD"))
+    private void onSetValue(String newText, CallbackInfo ci) {
+        updateTextMaxLength(newText);
+    }
+
+    @Inject(method = "onValueChange", at = @At("HEAD"))
+    private void onValueChange(String newText, CallbackInfo ci) {
+        updateTextMaxLength(newText);
+    }
+
+    @Unique
+    private void updateTextMaxLength(String newValue) {
+        if (!clientCommandLengthExtension) {
+            return;
+        }
+
+        boolean isClientcommandsCommand = false;
+        if (newValue.startsWith("/")) {
+            String[] commandArgs = newValue.substring(1).split(" ");
+            isClientcommandsCommand = commandArgs.length > 0 && ClientCommands.isClientcommandsCommand(commandArgs[0]);
+        }
+        if (isClientcommandsCommand) {
+            if (oldMaxLength == null) {
+                oldMaxLength = maxLength;
+            }
+            setMaxLength(32767);
+        } else {
+            // TODO: what if other mods try to do the same thing?
+            if (oldMaxLength != null) {
+                setMaxLength(oldMaxLength);
+                oldMaxLength = null;
+            }
+        }
+    }
 }
