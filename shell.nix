a24211e7c682a854e6ff1f5c264e93efe03e526c
Uriel <imurx@proton.me>
2024-06-23 15:23:01 +0200

    remove nix files

--- shell.nix
+++
@@ -1,2 +0,0 @@
-# Just uses the flake. For the nix-env addon (which is kind of dead) users, I use the direnv addon.
-(builtins.getFlake ("git+file://" + toString ./.)).devShells.${builtins.currentSystem}.default
