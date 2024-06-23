f113e8231715618dd634c03c55a25dc27f02f476
Uriel <imurx@proton.me>
2024-06-23 16:28:40 +0200

    add comments on why shift by -2

--- src/main/java/net/earthcomputer/clientcommands/mixin/commands/generic/ChatScreenMixin.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/commands/generic/ChatScreenMixin.java
@@ -12,7 +12,12 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public class ChatScreenMixin {
     // replace the text before the Fabric Command API executes it,
     // but ensure the message is added to the history in its raw form.
-    @ModifyVariable(method = "handleChatInput", at = @At(value = "INVOKE", target = "Ljava/lang/String;startsWith(Ljava/lang/String;)Z", remap = false, shift = At.Shift.BY, by = -2), argsOnly = true)
+
+    @ModifyVariable(
+            method = "handleChatInput",
+            // This is being shifted by -2 as the targeted variable gets loaded two instructions before the INVOKE
+            at = @At(value = "INVOKE", target = "Ljava/lang/String;startsWith(Ljava/lang/String;)Z", remap = false, shift = At.Shift.BY, by = -2),
+            argsOnly = true)
     private String onHandleChatInput(String message) {
         String prefix = AutoPrefixCommand.getCurrentPrefix();
         if (prefix == null || message.startsWith("/")) {
