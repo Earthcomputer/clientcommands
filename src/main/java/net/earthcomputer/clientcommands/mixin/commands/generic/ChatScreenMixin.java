41bb632cac372524837619a1d30e58fc96ab084a
Uriel <imurx@proton.me>
2024-06-23 16:39:01 +0200

    use `@ModifyReceiver` for mixin instead

--- src/main/java/net/earthcomputer/clientcommands/mixin/commands/generic/ChatScreenMixin.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/commands/generic/ChatScreenMixin.java
@@ -1,35 +1,34 @@
 package net.earthcomputer.clientcommands.mixin.commands.generic;
 
+import com.llamalad7.mixinextras.injector.ModifyReceiver;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.earthcomputer.clientcommands.ClientCommands;
 import net.earthcomputer.clientcommands.command.AutoPrefixCommand;
 import net.earthcomputer.clientcommands.command.VarCommand;
 import net.minecraft.client.gui.screens.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin {
     // replace the text before the Fabric Command API executes it,
     // but ensure the message is added to the history in its raw form.
-
-    @ModifyVariable(
-            method = "handleChatInput",
-            // This is being shifted by -2 as the targeted variable gets loaded two instructions before the INVOKE
-            at = @At(value = "INVOKE", target = "Ljava/lang/String;startsWith(Ljava/lang/String;)Z", remap = false, shift = At.Shift.BY, by = -2),
-            argsOnly = true)
-    private String onHandleChatInput(String message) {
+    @ModifyReceiver(method = "handleChatInput", at = @At(value = "INVOKE", target = "Ljava/lang/String;startsWith(Ljava/lang/String;)Z", remap = false))
+    private String onHandleChatInput(String instance, String slash, @Local(argsOnly = true) LocalRef<String> message) {
         String prefix = AutoPrefixCommand.getCurrentPrefix();
-        if (prefix == null || message.startsWith("/")) {
+        if (prefix == null || instance.startsWith("/")) {
             prefix = "";
         } else {
             prefix = prefix + " ";
         }
 
-        String command = VarCommand.replaceVariables(prefix + message);
+        String command = VarCommand.replaceVariables(prefix + instance);
         if (command.startsWith("/")) {
             ClientCommands.sendCommandExecutionToServer(command.substring(1));
         }
+
+        message.set(command);
         return command;
     }
 }
