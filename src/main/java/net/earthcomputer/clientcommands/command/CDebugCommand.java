3937d3371d1f5ce1d9b1e7a3236405f379671ed1
lugosieben <62535192+lugosieben@users.noreply.github.com>
2024-06-24 20:09:20 +0200

    Implement requested changes

--- src/main/java/net/earthcomputer/clientcommands/command/CDebugCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/CDebugCommand.java
@@ -1,34 +1,34 @@
 package net.earthcomputer.clientcommands.command;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.*;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.DebugScreenOverlay;
 
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
 
 public class CDebugCommand {
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(literal("cdebug")
                 .executes((context -> execute("overlay")))
-                .then(literal("fps").executes(context -> execute("fps")))
-                .then(literal("network").executes(context -> execute("network")))
-                .then(literal("profiler").executes(context -> execute("profiler")))
+                .then(literal("overlay")
+                        .executes(context -> execute("overlay")))
+                .then(literal("fps")
+                        .executes(context -> execute("fps")))
+                .then(literal("network")
+                        .executes(context -> execute("network")))
+                .then(literal("profiler")
+                        .executes(context -> execute("profiler")))
         );
     }
 
     private static int execute(String type) {
         DebugScreenOverlay debugScreenOverlay = Minecraft.getInstance().getDebugOverlay();
         switch (type) {
-            case "overlay":
-                debugScreenOverlay.toggleOverlay(); break;
-            case "fps":
-                debugScreenOverlay.toggleFpsCharts(); break;
-            case "network":
-                debugScreenOverlay.toggleNetworkCharts(); break;
-            case "profiler":
-                debugScreenOverlay.toggleProfilerChart(); break;
+            case "overlay" -> debugScreenOverlay.toggleOverlay();
+            case "fps" -> debugScreenOverlay.toggleFpsCharts();
+            case "network" -> debugScreenOverlay.toggleNetworkCharts();
+            case "profiler" -> debugScreenOverlay.toggleProfilerChart();
         }
         return Command.SINGLE_SUCCESS;
     }
