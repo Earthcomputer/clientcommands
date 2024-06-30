1846f71fc237703ab876df325ea10ae54c435d44
Frederik van der Els <49305700+xpple@users.noreply.github.com>
2024-06-30 13:25:15 +0200

    Change continuation indent to 4

--- src/main/java/net/earthcomputer/clientcommands/command/CDebugCommand.java
+++ src/main/java/net/earthcomputer/clientcommands/command/CDebugCommand.java
@@ -11,16 +11,15 @@ import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
 public class CDebugCommand {
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(literal("cdebug")
-                .executes((context -> execute("overlay")))
-                .then(literal("overlay")
-                        .executes(context -> execute("overlay")))
-                .then(literal("fps")
-                        .executes(context -> execute("fps")))
-                .then(literal("network")
-                        .executes(context -> execute("network")))
-                .then(literal("profiler")
-                        .executes(context -> execute("profiler")))
-        );
+            .executes(context -> execute("overlay"))
+            .then(literal("overlay")
+                .executes(context -> execute("overlay")))
+            .then(literal("fps")
+                .executes(context -> execute("fps")))
+            .then(literal("network")
+                .executes(context -> execute("network")))
+            .then(literal("profiler")
+                .executes(context -> execute("profiler"))));
     }
 
     private static int execute(String type) {
