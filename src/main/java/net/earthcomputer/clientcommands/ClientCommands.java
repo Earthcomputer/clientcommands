9edf4411a993bd63173c312b23e90955bfc97d0b
Frederik van der Els <frederik.v.d.els@outlook.com>
2024-09-16 14:39:16 +0200

    Update BetterConfig

--- src/main/java/net/earthcomputer/clientcommands/ClientCommands.java
+++ src/main/java/net/earthcomputer/clientcommands/ClientCommands.java
@@ -72,7 +72,7 @@ public class ClientCommands implements ClientModInitializer {
             LOGGER.error("Failed to create config dir", e);
         }
 
-        new ModConfigBuilder("clientcommands", Configs.class).build();
+        new ModConfigBuilder<>("clientcommands", Configs.class).build();
         ClientConnectionEvents.DISCONNECT.register(() -> {
             if (!Relogger.isRelogging) {
                 BetterConfigAPI.getInstance().getModConfig("clientcommands").resetTemporaryConfigs();
