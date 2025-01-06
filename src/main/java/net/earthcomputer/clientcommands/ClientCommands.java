a3d358ed4dacca1e63a5382b06af87468876dcc2
Frederik van der Els <frederik.v.d.els@outlook.com>
2025-01-06 19:14:13 +0100

    Move comment and add explanation

--- src/main/java/net/earthcomputer/clientcommands/ClientCommands.java
+++ src/main/java/net/earthcomputer/clientcommands/ClientCommands.java
@@ -1,3 +1,4 @@
+// CHECKSTYLE:OFF: AvoidStarImport allow commands to be wildcard imported
 package net.earthcomputer.clientcommands;
 
 import com.mojang.brigadier.CommandDispatcher;
@@ -6,9 +7,7 @@ import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.mojang.logging.LogUtils;
 import dev.xpple.betterconfig.api.BetterConfigAPI;
 import dev.xpple.betterconfig.api.ModConfigBuilder;
-// CHECKSTYLE:OFF: AvoidStarImport
 import net.earthcomputer.clientcommands.command.*;
-// CHECKSTYLE:ON: AvoidStarImport
 import net.earthcomputer.clientcommands.event.ClientConnectionEvents;
 import net.earthcomputer.clientcommands.features.CommandExecutionCustomPayload;
 import net.earthcomputer.clientcommands.features.FishingCracker;
