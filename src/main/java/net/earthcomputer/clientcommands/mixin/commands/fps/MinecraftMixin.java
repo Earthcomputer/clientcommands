c05edc588f86dab98a0d4a386519481c74052a81
Wagyourtail <wagyourtail@wagyourtail.xyz>
2025-03-24 15:11:36 -0500

    fix import

--- src/main/java/net/earthcomputer/clientcommands/mixin/commands/fps/MinecraftMixin.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/commands/fps/MinecraftMixin.java
@@ -2,6 +2,7 @@ package net.earthcomputer.clientcommands.mixin.commands.fps;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
