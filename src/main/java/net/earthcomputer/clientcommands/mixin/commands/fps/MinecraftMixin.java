d0c481f38c6519ae1c8a4ae63615d2fa255cc950
Wagyourtail <wagyourtail@wagyourtail.xyz>
2025-03-24 14:53:47 -0500

    simplify mixins

--- src/main/java/net/earthcomputer/clientcommands/mixin/commands/fps/MinecraftMixin.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/commands/fps/MinecraftMixin.java
@@ -1,27 +1,16 @@
 package net.earthcomputer.clientcommands.mixin.commands.fps;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
 
-    // make sure if the fps isn't our new unlimited value, then set it to 259 so that it still gets limited
-    @ModifyExpressionValue(method = "runTick", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/FramerateLimitTracker;getFramerateLimit()I"))
-    private int higherFpsCap(int original, @Share("realMaxFps") LocalIntRef maxFps) {
-        maxFps.set(original);
-        return original > 260 && original != Integer.MAX_VALUE ? 259 : original;
-    }
-
-    // set the limiter to the real max fps
-    @ModifyArg(method = "runTick", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;limitDisplayFPS(I)V", remap = false))
-    private int setToRealFps(int fps, @Share("realMaxFps") LocalIntRef maxFps) {
-        return maxFps.get();
+    @ModifyExpressionValue(method = "runTick", at = @At(value = "CONSTANT", args = "intValue=260"))
+    private int fixMaxFps(int fps) {
+        return Integer.MAX_VALUE;
     }
 
 }
--- src/main/java/net/earthcomputer/clientcommands/mixin/commands/fps/OptionsMixin.java
+++ src/main/java/net/earthcomputer/clientcommands/mixin/commands/fps/OptionsMixin.java
@@ -8,10 +8,9 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(Options.class)
 public class OptionsMixin {
 
-    // make it set it to the new unlimited value set in MinecraftMixin
     @ModifyArg(method = "lambda$new$8", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/FramerateLimitTracker;setFramerateLimit(I)V"))
     private static int fixMaxFpsSet(int maxFps) {
-        if (maxFps == 260) {
+        if (maxFps == Options.UNLIMITED_FRAMERATE_CUTOFF) {
             return Integer.MAX_VALUE;
         }
         return maxFps;
