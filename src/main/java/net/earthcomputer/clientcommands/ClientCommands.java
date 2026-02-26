cc7722dec4a2c7429f4c4d6f290232b94fc9daaa
Frederik van der Els <fred@xpple.dev>
2026-02-26 21:39:16 +0100

    Add cwindowsize command

--- src/main/java/net/earthcomputer/clientcommands/ClientCommands.java
+++ src/main/java/net/earthcomputer/clientcommands/ClientCommands.java
@@ -195,6 +195,7 @@ public class ClientCommands implements ClientModInitializer {
         WeatherCommand.register(dispatcher);
         WhisperEncryptedCommand.register(dispatcher);
         WikiCommand.register(dispatcher);
+        WindowSizeCommand.register(dispatcher);
 
         Calendar calendar = Calendar.getInstance();
         boolean registerChatCommand = calendar.get(Calendar.MONTH) == Calendar.APRIL && calendar.get(Calendar.DAY_OF_MONTH) == 1;
---
+++ src/main/java/net/earthcomputer/clientcommands/command/WindowSizeCommand.java
@@ -0,0 +1,42 @@
+package net.earthcomputer.clientcommands.command;
+
+import com.mojang.blaze3d.opengl.GlStateManager;
+import com.mojang.blaze3d.platform.Window;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.CommandDispatcher;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import org.lwjgl.glfw.GLFW;
+
+import static com.mojang.brigadier.arguments.IntegerArgumentType.*;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
+
+public class WindowSizeCommand {
+    public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
+        dispatcher.register(literal("cwindowsize")
+            .then(argument("width", integer(0))
+                .then(argument("height", integer(0))
+                    .executes(ctx -> setWindowSize(ctx.getSource(), getInteger(ctx, "width"), getInteger(ctx, "height"))))));
+    }
+
+    private static int setWindowSize(FabricClientCommandSource source, int width, int height) {
+        Window window = source.getClient().getWindow();
+        long handle = window.handle();
+
+        int oldX = window.getX();
+        int oldY = window.getY();
+        int oldWidth = window.getWidth();
+        int oldHeight = window.getHeight();
+
+        int centerX = oldX + oldWidth / 2;
+        int centerY = oldY + oldHeight / 2;
+
+        GLFW.glfwSetWindowSize(handle, width, height);
+
+        int newX = centerX - width / 2;
+        int newY = centerY - height / 2;
+
+        GlStateManager._viewport(newX, newY, width, height);
+        GLFW.glfwSetWindowPos(handle, newX, newY);
+        return Command.SINGLE_SUCCESS;
+    }
+}
